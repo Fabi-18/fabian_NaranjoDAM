b9049881b9288f51956edcd96bcf7393800a04d1
fnaranjoc06 <fnaranjoc06@iesalbarregas.es>
2021-03-15 23:28:35 +0100

    Validamos la clase principal 1º commit

---
+++ fabian_NaranjoDAM/src/fabian_NaranjoDAM/Principal.java
@@ -0,0 +1,11 @@
+package fabian_NaranjoDAM;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Hola");
+
+	}
+
+}
