fd6f4d3e23f586edc72d1f74efafa5daf2ac20ba
fnaranjoc06 <fnaranjoc06@iesalbarregas.es>
2021-03-16 09:52:43 +0100

    Líneas en método main.

--- fabian_NaranjoDAM/src/fabian_NaranjoDAM/Principal.java
+++ fabian_NaranjoDAM/src/fabian_NaranjoDAM/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-		
-
+		System.out.println("Esta es la primera línea que se muestra por pantalla");
+		System.out.println("Y esta es la segunda línea que se muestra por pantalla");
 	}
 
 }
